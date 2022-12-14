ee76dfeb3e01ba3fed505f6398ee24653809c7c6
Mads Smed <msth@trifork.com>
2022-12-14 16:06:59 +0100

    Add chakra ui and theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,15 @@ import { type AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../styles/theme";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default trpc.withTRPC(MyApp);
